3cf48603bb1c312a5dfbfaea282e5fdc8916713e
shareeshk <shareeshk@gmail.com>
2026-01-30 22:11:54 +0530

    test [deploy]

--- _data/site.js
+++ _data/site.js
@@ -2,5 +2,5 @@ module.exports = {
   strapiUrl:
     process.env.ELEVENTY_ENV === "production"
       ? "http://voometstudio.com/eon-strapi"
-      : "http://localhost:1337",
+      : "http://voometstudio.com/eon-strapi",
 };
