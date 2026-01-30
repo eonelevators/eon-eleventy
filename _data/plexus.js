d346be2b280bdb8e4f1a69e9796668cfb92bea61
manjurshv <manjurshv@gmail.com>
2026-01-30 15:42:43 +0530

    Update plexus.js

--- _data/plexus.js
+++ _data/plexus.js
@@ -3,15 +3,15 @@ const fetch = require("node-fetch");
 module.exports = async () => {
   try {
     const response = await fetch(
-      "http://localhost:1337/api/products?filters[slug][$eq]=product-plexus&populate[sections][populate]=*"
+      "http://voometstudio.com/eon-strapi/api/products?filters[slug][$eq]=product-plexus&populate[sections][populate]=*"
     );
 
     const json = await response.json();
 
-    console.log(
-      "Sections array:",
-      JSON.stringify(json.data[0].sections, null, 2)
-    );
+    //console.log(
+      //"Sections array:",
+      //JSON.stringify(json.data[0].sections, null, 2)
+   // );
 
     return json.data[0];
 
