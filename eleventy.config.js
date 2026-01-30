6e133d2b4dba2ede1cabf599a93f418ba0995486
shareeshk <shareeshk@gmail.com>
2026-01-30 19:44:54 +0530

    testvv [deploy]

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("assets"); // 👈 Copies CSS to _site
   return {
+	pathPrefix: "/eon-eleventy/",
     dir: {
       input: ".",
       output: "_site"
